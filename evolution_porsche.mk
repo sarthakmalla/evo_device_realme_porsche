acbb450581ec0a8784cec4c66f0186cb46b3d508
sarthak malla <sarthakmalla17@gmail.com>
2023-09-17 13:28:04 +0530

    evo-ify

--- evolution_porsche.mk
+++ evolution_porsche.mk
@@ -11,18 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from porsche device
 $(call inherit-product, device/realme/porsche/device.mk)
 
-# Inherit some common AwakenOS stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-# AwakenOS Extras
-AWAKEN_BUILD_TYPE := official
+# Evolution X Extras
+EVO_BUILD_TYPE := Testing_Build
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_USES_MINI_GAPPS := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
-PRODUCT_NAME := awaken_porsche
+PRODUCT_NAME := evolution_porsche
 PRODUCT_DEVICE := porsche
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
